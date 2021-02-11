4fc02ab03d9f1c349b2da3da5feebb87fdcf4a97
Olli Karppinen <olli.karppinen@relex.fi>
2021-02-11 22:26:52 +0200

    Adds 5.5 solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import Blog from "./components/Blog"
 import blogService from "./services/blogs"
 import loginService from "./services/login"
 import "./App.css"
+import Togglable from "./components/Togglable"
 
 const App = () => {
   const [username, setUsername] = useState("")
@@ -27,6 +28,8 @@ const App = () => {
       blogService.setToken(user.token)
     }
   }, [])
+
+  const blogFormRef = useRef()
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -58,6 +61,7 @@ const App = () => {
     event.preventDefault()
     try {
       const createdBlog = await blogService.create({ title, author, url })
+      blogFormRef.current.toggleVisibility()
       setBlogs(blogs.concat(createdBlog))
       setTitle(null)
       setAuthor(null)
@@ -111,7 +115,9 @@ const App = () => {
       <p>
         {user.name} logged in <button onClick={handleLogout}>log out</button>
       </p>
-      {newBlogForm()}
+      <Togglable buttonLabel="new note" ref={blogFormRef}>
+        {newBlogForm()}
+      </Togglable>
       <div>
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
