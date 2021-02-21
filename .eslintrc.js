1dccde4caf6859be9f0c313e1591bdbe82ec1fce
Olli Karppinen <olli.karppinen@relex.fi>
2021-02-21 12:27:11 +0200

    Updates dependencies and adds react eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,4 +17,22 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {},
+  settings: {
+    react: {
+      createClass: "createReactClass",
+      pragma: "React",
+      fragment: "Fragment",
+      version: "detect",
+      flowVersion: "0.53"
+    },
+    propWrapperFunctions: [
+        "forbidExtraProps",
+        {"property": "freeze", "object": "Object"},
+        {"property": "myFavoriteWrapper"}
+    ],
+    linkComponents: [
+      "Hyperlink",
+      {"name": "Link", "linkAttribute": "to"}
+    ]
+  }
 }
