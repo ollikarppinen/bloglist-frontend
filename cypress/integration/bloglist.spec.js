a0e84105611f1b4d8e7b887388a75d189264c069
Olli Karppinen <olli.karppinen@relex.fi>
2021-02-23 21:18:53 +0200

    Adds 5.18 solution

--- cypress/integration/bloglist.spec.js
+++ cypress/integration/bloglist.spec.js
@@ -1,10 +1,39 @@
 describe("Blog app", function() {
   beforeEach(function() {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      name: "Matti Luukkainen",
+      username: "mluukkai",
+      password: "salainen",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3000");
   });
 
-  it("Login form is shown", function() {
-    cy.contains("log in to application");
+  describe("when not logged in", function() {
+    it("login form is shown", function() {
+      cy.contains("log in to application");
+    });
+  });
+
+  describe("when logging in", function() {
+    it("succeeds with correct credentials", function() {
+      cy.get("#username").type("mluukkai");
+      cy.get("#password").type("salainen");
+      cy.get("#login-button").click();
+
+      cy.contains("blogs");
+    });
+
+    it("fails with wrong credentials", function() {
+      cy.get("#username").type("mluukkai");
+      cy.get("#password").type("wrong");
+      cy.get("#login-button").click();
+
+      cy.get(".error")
+        .should("contain", "Wrong credentials")
+        .and("have.css", "color", "rgb(255, 0, 0)")
+        .and("have.css", "border-style", "solid");
+    });
   });
 });
